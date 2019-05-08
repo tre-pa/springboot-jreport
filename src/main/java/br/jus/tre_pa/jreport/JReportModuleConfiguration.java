0f6f0eac46763d6eed1f05ea304dae786ebd4fac
jcruz <jcruz@tre-pa.jus.br>
2019-05-08 14:30:43 -0300

    Add @AutoConfigurationPackage no módulo

--- src/main/java/br/jus/tre_pa/jreport/JReportModuleConfiguration.java
+++ src/main/java/br/jus/tre_pa/jreport/JReportModuleConfiguration.java
@@ -1,10 +1,12 @@
 package br.jus.tre_pa.jreport;
 
+import org.springframework.boot.autoconfigure.AutoConfigurationPackage;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @ComponentScan(basePackages = "br.jus.tre_pa.jreport")
+@AutoConfigurationPackage
 public class JReportModuleConfiguration {
 
 }
